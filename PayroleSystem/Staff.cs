917b107937acd189048bffdbf13420e4785c3904
MathewBaldwin <moretwomusic@gmail.com>
2017-05-22 22:57:06 -0400

    First Commit

---
+++ PayroleSystem/Staff.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayroleSystem
+{
+    class Staff
+    {
+        private float hourlyRate;
+
+        private int hWorked;
+
+        public float BasicPay { get; private set; }
+
+        public float TotalPay { get; protected set; }
+
+        public string NameOfStaff { get; private set; }
+
+        public float HoursWorked
+        {
+            get { return hWorked; }
+            set
+            {
+                if (value > 0)
+                    hWorked = (int)value;
+                else
+                    hWorked = 0;
+            }
+        }
+
+        public Staff(string name, float rate)
+        {
+            name = NameOfStaff;
+
+            rate = hourlyRate;
+        }
+
+        public virtual void CalculatePay()
+        {
+            Console.WriteLine("Calculating Pay...");
+
+            BasicPay = hWorked * hourlyRate;
+
+            TotalPay = BasicPay;
+        }
+
+        public override string ToString()
+        {
+            return "Name of Staff = " + NameOfStaff + ", hourlyRate = " + hourlyRate + ", hWorked = " + hWorked + ", BasicPay = " + BasicPay + ", TotalPay = " + TotalPay + ", HoursWorked = " + HoursWorked;
+        }
+    }
+}
